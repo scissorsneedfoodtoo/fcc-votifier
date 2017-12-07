a0840212be9388a2ef83286ad944d4e4bf5be3eb
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-12-07 21:27:15 +0900

    working through Auth0 tutorial to get the hang of routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import CelebrityJokes from './components/CelebrityJokes';
+import FoodJokes from './components/FoodJokes';
+import { Router, Route, Switch } from 'react-router';
+import { createHashHistory } from 'history'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createHashHistory()
+
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={history}>
+        <Switch>
+          <Route path="/" component={FoodJokes}/>
+          <Route path="/special" component={CelebrityJokes}/>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
