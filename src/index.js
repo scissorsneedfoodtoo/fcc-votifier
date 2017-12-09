86555549cee4b409b824d0ef5d2e27c8e372c168
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-12-09 17:13:42 +0900

    trying to suss out the differences between react-router-dom 3 used in the tutorial and the newer v4. got basic page functionality

--- src/index.js
+++ src/index.js
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom';
 import CelebrityJokes from './components/CelebrityJokes';
 import FoodJokes from './components/FoodJokes';
 import { Router, Route, Switch } from 'react-router';
-import { createHashHistory } from 'history'
+import { HashRouter } from 'react-router-dom'; // for static sites with known files as in the tutorial
+import { BrowserRouter } from 'react-router-dom'; // for use when we have a server with dynamic requests -- usually preferable
 import registerServiceWorker from './registerServiceWorker';
-
-const history = createHashHistory()
 
 const Root = () => {
   return (
     <div className="container">
-      <Router history={history}>
+      <HashRouter>
         <Switch>
-          <Route path="/" component={FoodJokes}/>
+          <Route exact path="/" component={FoodJokes}/>
           <Route path="/special" component={CelebrityJokes}/>
         </Switch>
-      </Router>
+      </HashRouter>
     </div>
   )
 }
