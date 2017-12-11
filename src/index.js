13a2429601ac29040533520175da4667ad913fbd
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-12-11 21:25:07 +0900

    basic login and callback seems to work properly. But still can't get authorization to view chuck norris jokes. Need to work out the history or jwt being passed to header

--- src/index.js
+++ src/index.js
@@ -2,27 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CelebrityJokes from './components/CelebrityJokes';
 import FoodJokes from './components/FoodJokes';
-import { Router, Route, Switch } from 'react-router';
+import { Route, Switch } from 'react-router';
 // import { HashRouter } from 'react-router-dom'; // for static sites with known files as in the tutorial
-// import { BrowserRouter } from 'react-router-dom'; // for use when we have a server with dynamic requests -- usually preferable
+import { BrowserRouter } from 'react-router-dom'; // for use when we have a server with dynamic requests -- usually preferable
 import { createHashHistory } from 'history'
 import registerServiceWorker from './registerServiceWorker';
 import Callback from './components/Callback';
 import { requireAuth } from './utils/AuthService';
-// require('dotenv').config();
 
 const history = createHashHistory()
 
 const Root = () => {
   return (
     <div className="container">
-      <Router history={history}>
+      <BrowserRouter history={history}>
         <Switch>
           <Route exact path="/" component={FoodJokes}/>
           <Route path="/special" component={CelebrityJokes} onEnter={requireAuth}/>
           <Route path="/callback" component={Callback} />
         </Switch>
-      </Router>
+      </BrowserRouter>
     </div>
   )
 }
